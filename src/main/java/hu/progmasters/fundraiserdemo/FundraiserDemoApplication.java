9505c56a53e54de91c733af0448887fdc3191200
gyarmatiadam <gyari12@gmail.com>
2023-11-16 10:54:01 +0100

    init2

---
+++ src/main/java/hu/progmasters/fundraiserdemo/FundraiserDemoApplication.java
@@ -0,0 +1,13 @@
+package hu.progmasters.fundraiserdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundraiserDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundraiserDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/progmasters/fundraiserdemo/FundraiserDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmasters.fundraiserdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraiserDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
